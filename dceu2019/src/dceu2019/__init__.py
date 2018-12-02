6e025b88a49842ea116204a29b2d7e5161f94652
Benjamin Bach <benjamin@overtag.dk>
2018-12-02 13:40:23 +0100

    Move static site to separate subfolder + bootstrap django project w/ @keimlink 's lovely project https://github.com/keimlink/django-project-package-template

---
+++ dceu2019/src/dceu2019/__init__.py
@@ -0,0 +1,2 @@
+__project_name__ = "dceu2019"
+__version__ = "0.1.0"
---
+++ dceu2019/src/dceu2019/conf/urls.py
@@ -0,0 +1,28 @@
+"""
+dceu2019 URL Configuration.
+
+The ``urlpatterns`` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+
+Examples:
+    Function views
+        1. Add an import: from dceu2019.apps.my_app import views
+        2. Add a URL to urlpatterns: path("", views.home, name="home")
+    Class-based views
+        1. Add an import: from dceu2019.apps.other_app.views import Home
+        2. Add a URL to urlpatterns: path("", Home.as_view(), name="home")
+    Including another URLconf
+        1. Import the include() function: from django.urls import include, path
+        2. Add a URL to urlpatterns: path("blog/", include("dceu2019.apps.blog.urls"))
+
+"""
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [path("admin/", admin.site.urls)]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
