b0a82cdee7ec84eb50a82b0c132b732af3d2b05a
Benjamin Bach <benjamin@overtag.dk>
2019-02-17 18:52:42 +0100

    Add score and only stringify things that are not numbers

--- dceu2019/src/dceu2019/apps/pretalx_utils/management/commands/pretalx_submissions2csv.py
+++ dceu2019/src/dceu2019/apps/pretalx_utils/management/commands/pretalx_submissions2csv.py
@@ -16,11 +16,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         field_names = [f.name for f in models.Submission._meta.fields]
-        field_names = ["speakers"] + field_names
         writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
-        writer.writerow(field_names)
+        writer.writerow(
+            ["speakers", "score"] +
+            field_names
+        )
         for instance in models.Submission.objects.all():
-            writer.writerow(
-                [", ".join([person.get_display_name() for person in instance.speakers.all()])] +
-                [str(getattr(instance, f)).encode('utf-8') for f in field_names]
-            )
+            values = []
+            speakers = ", ".join([person.get_display_name() for person in instance.speakers.all()])
+            score = instance.average_score
+            values.append(speakers)
+            values.append(score)
+            for f in field_names:
+                value = getattr(instance, f)
+                if not isinstance(value, (int, float)):
+                    values.append(str(value).encode('utf-8'))
+                else:
+                    values.append(value)
+            writer.writerow(values)
